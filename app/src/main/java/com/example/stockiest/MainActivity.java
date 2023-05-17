09543b7d7c4e4416bf522927926f4dd07a1bd85e
Nicholas Grimaldi <nicholasgrimaldi42@gmail.com>
2023-05-17 11:54:28 -0400

    fixed notification prompt

--- app/src/main/java/com/example/stockiest/MainActivity.java
+++ app/src/main/java/com/example/stockiest/MainActivity.java
@@ -2,16 +2,20 @@ package com.example.stockiest;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 
 import android.content.SharedPreferences;
@@ -39,16 +43,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final ActivityResultLauncher<String> pushNotificationPermissionLauncher = registerForActivityResult(
-            new ActivityResultContracts.RequestPermission(), isGranted -> {
-                if (isGranted) {
-                    Toast.makeText(MainActivity.this, "Notification permission granted.", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Notification permission denied.", Toast.LENGTH_SHORT).show();
-                }
-            });
-
     private Switch newsSwitch;
     private Switch earningsSwitch;
     private SharedPreferences prefs;
@@ -57,6 +51,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Launch the notification permission request if it hasn't been granted
+        if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 0);
+        }
 
         com.example.stockiest.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -81,14 +80,6 @@ public class MainActivity extends AppCompatActivity {
             notificationManager.createNotificationChannel(channel);
         }
 
-        // Request permission to show notifications
-        requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 0);
-
-        // Launch the notification permission request if it hasn't been granted
-        if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            pushNotificationPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS);
-        }
-
         newsSwitch = findViewById(R.id.switch1);
         earningsSwitch = findViewById(R.id.switch2);
         prefs = getSharedPreferences("myPrefs", MODE_PRIVATE);
@@ -98,79 +89,74 @@ public class MainActivity extends AppCompatActivity {
         boolean isEarningsSwitchOn = prefs.getBoolean("earningsSwitchOn", false);
 
         newsSwitch.setChecked(isNewsSwitchOn);
-        newsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Save the new switch state to SharedPreferences
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putBoolean("newsSwitchOn", isChecked);
-                editor.apply();
+        newsSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Save the new switch state to SharedPreferences
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("newsSwitchOn", isChecked);
+            editor.apply();
 
-                Intent serviceIntent = new Intent(MainActivity.this, StockiestService.class);
+            Intent serviceIntent = new Intent(MainActivity.this, StockiestService.class);
 
-                if (isChecked) {
-                    System.out.println("it's checked! Check to see if this printed!");
-
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        startForegroundService(serviceIntent);
-                    } else {
-                        startService(serviceIntent);
-                    }
-                } else {
-                    stopService(serviceIntent);
-                }
-            }
+            serviceHelper(isChecked, serviceIntent);
         });
-        earningsSwitch.setChecked(isEarningsSwitchOn);
-        earningsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // Save the new switch state to SharedPreferences
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putBoolean("earningsSwitchOn", isChecked);
-                editor.apply();
 
-                if (isChecked) {
-                    Intent serviceIntent = new Intent(MainActivity.this, StockiestService.class);
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        startForegroundService(serviceIntent);
-                    } else {
-                        startService(serviceIntent);
-                    }
-                } else {
-                    Intent serviceIntent = new Intent(MainActivity.this, StockiestService.class);
-                    stopService(serviceIntent);
-                }
-            }
+        earningsSwitch.setChecked(isEarningsSwitchOn);
+        earningsSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            // Save the new switch state to SharedPreferences
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("earningsSwitchOn", isChecked);
+            editor.apply();
+
+            Intent serviceIntent = new Intent(MainActivity.this, StockiestService.class);
+
+            serviceHelper(isChecked, serviceIntent);
         });
     }
 
+    /**
+     * Callback to handle result of user accepting or denying
+     * notification permissions
+     *
+     * @param requestCode The request code passed in {@link #requestPermissions(
+     * android.app.Activity, String[], int)}
+     * @param permissions The requested permissions. Never null.
+     * @param grantResults The grant results for the corresponding permissions
+     *     which is either {@link android.content.pm.PackageManager#PERMISSION_GRANTED}
+     *     or {@link android.content.pm.PackageManager#PERMISSION_DENIED}. Never null.
+     *
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-    CompoundButton.OnCheckedChangeListener switchListener = new CompoundButton.OnCheckedChangeListener() {
-        @Override
-        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-            // Save the new switch state to SharedPreferences
-            SharedPreferences.Editor editor = prefs.edit();
-            if (buttonView == newsSwitch) {
-                editor.putBoolean("newsSwitchOn", isChecked);
-            } else if (buttonView == earningsSwitch) {
-                editor.putBoolean("earningsSwitchOn", isChecked);
-            }
-            editor.apply();
-
-            // Start or stop the service based on the switch state
-            Intent serviceIntent = new Intent(MainActivity.this, StockiestService.class);
-            if (isChecked) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    startForegroundService(serviceIntent);
-                } else {
-                    startService(serviceIntent);
-                }
+        if (requestCode == 0) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted
+                Toast.makeText(MainActivity.this, "Notification permission granted.", Toast.LENGTH_SHORT).show();
             } else {
-                stopService(serviceIntent);
+                // Permission denied
+                Toast.makeText(MainActivity.this, "Notification permission denied.", Toast.LENGTH_SHORT).show();
             }
         }
-    };
+    }
+
+    /**
+     * Method to reduce repetitive code
+     *
+     * @param isChecked Boolean representing the state of the given switch
+     * @param serviceIntent Intent Object meant to be started or stopped
+     */
+    private void serviceHelper(boolean isChecked, Intent serviceIntent) {
+        if (isChecked) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(serviceIntent);
+            } else {
+                startService(serviceIntent);
+            }
+        } else {
+            stopService(serviceIntent);
+        }
+    }
 }
 
 
